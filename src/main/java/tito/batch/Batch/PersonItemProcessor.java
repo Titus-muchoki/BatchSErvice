0d8d14492df8fb2787fe5c4baf5e87922d49b770
kajela <titoyut56@gmail.com>
2023-09-14 19:48:32 +0300

    add the initial files

---
+++ src/main/java/tito/batch/Batch/PersonItemProcessor.java
@@ -0,0 +1,32 @@
+package tito.batch.Batch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
+public class PersonItemProcessor implements ItemProcessor<Person, Person> {
+
+    private static final Logger log = LoggerFactory.getLogger(PersonItemProcessor.class);
+
+    @Override
+    public Person process(final Person person) throws Exception {
+        final String firstName = person.getFirstName().toUpperCase();
+        final String lastName = person.getLastName().toUpperCase();
+
+        final Person transformedPerson = new Person(firstName, lastName);
+
+        log.info("Converting (" + person + ") into (" + transformedPerson + ")");
+
+        return transformedPerson;
+    }
+
+}
